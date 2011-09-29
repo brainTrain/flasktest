6bc54668149bf8ff78cba06af403ee59d4c87ebb
Brain Train <abductee@gmail.com>
2011-09-28 22:21:14 -0700

    fad

--- static/js/pusher-test.js
+++ static/js/pusher-test.js
@@ -1,7 +1,7 @@
-var pusher = new Pusher('a84f060526adcba703cc');    
-    var channel = pusher.subscribe('alert');
+var pusher = new Pusher('0b75eb1a823194806706');    
+    var channel = pusher.subscribe('blah');
 
-    channel.bind('chirp', function(data) {  //listens for 'chirp' events on my channel 'alert'
+    channel.bind('click', function(data) {  //listens for 'chirp' events on my channel 'alert'
 
         alert(data);
 
